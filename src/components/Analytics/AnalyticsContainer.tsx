1dbcd14b573c7ddea9fdec24fb2b17e924862814
NTiger07 <falaleru@gmail.com>
2023-10-24 23:16:16 +0100

    Mocking Analytics Layout WIP-0

--- src/components/Analytics/AnalyticsContainer.tsx
+++ src/components/Analytics/AnalyticsContainer.tsx
@@ -1,14 +1,39 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 const AnalyticsContainer = () => {
   const storedTrashListJSON = localStorage.getItem("trashList");
   const storedTrashList = JSON.parse(storedTrashListJSON)
 
+  const [trashList, setTrashList] = useState(storedTrashList || [])
 
-  console.log(storedTrashList)
+
+
+  console.log(trashList)
 
   return (
-    <div>AnalyticsContainer</div>
+    <div className="h-[100vh] mx-[2%] mt-[2%] flex flex-col gap-2">
+      <div className="SEARCH h-[10vh]">
+        Header and account
+      </div>
+
+
+
+
+      <div className="MAINSECTION h-[85vh] flex flex-col">
+
+
+        <div className="MAINGRAPHS flex flex-row items-cente h-[60%] gap-4">
+          <div className="TRENDS w-[75%] h-[50%] bg-[#37474F] rounded-lg shadow-lg"></div>
+          <div className="TYPES_PIE w-[30%] h-[50%] bg-[#37474F] rounded-lg shadow-lg"></div>
+        </div>
+
+
+        <div className="h-[40%] bg-primary-red"></div>
+
+
+
+      </div>
+    </div>
   )
 }
 

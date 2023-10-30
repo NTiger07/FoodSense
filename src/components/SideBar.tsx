8a000b36c2c8cf5059b3f9b28db6aba1a22b7cf1
NTiger07 <falaleru@gmail.com>
2023-10-30 15:02:33 +0100

    Added Logout button

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,9 +5,10 @@ const SideBar = () => {
     const navigate = useNavigate()
     const [isSidebarExpanded, setIsSideBarExpanded] = useState(false)
 
+
     return (
         <div className={`relative bg-[#37474F] transition-all h-[100vh] w-[100%] flex flex-col shadow-xl overflow-clip pt-[30%] pb-[5rem] ${isSidebarExpanded ? "w-[15rem] top-0 absolute z-20 h-[100vh]" : ""}`}>
-            <div className="w-full flex justify-center mb-[12rem] cursor-pointer" onClick={() => setIsSideBarExpanded(!isSidebarExpanded)}>
+            <div className="w-full flex justify-center mb-[6rem] cursor-pointer" onClick={() => setIsSideBarExpanded(!isSidebarExpanded)}>
                 <img src="/assets/menu-white.svg" className="w-[1.8rem]" alt="" />
             </div>
             <div className="flex flex-col w-full items-center justify-center gap-[3rem]">
@@ -30,6 +31,10 @@ const SideBar = () => {
                 <div className={`flex flex-row items-center cursor-pointer w-[70%] gap-2 ${isSidebarExpanded ? "" : "w-full justify-center"}`} onClick={() => navigate("/education")}>
                     <img src="/assets/education-white.svg" className="w-[1.8rem]" alt="" />
                     <span className={`w-[100%] text-left ${isSidebarExpanded ? "text-[1.2rem] font-semibold" : "hidden"}`}>Education</span>
+                </div>
+                <div className={`mt-[5rem] flex flex-row items-center cursor-pointer w-[70%] gap-2 ${isSidebarExpanded ? "" : "w-full justify-center"}`} onClick={() => window.location.href = "http://localhost:3000/auth/logout"}>
+                    <img src="/assets/logout.svg" className="w-[1.8rem]" alt="" />
+                    <span className={`w-[100%] text-left ${isSidebarExpanded ? "text-[1.2rem] font-semibold" : "hidden"}`}>Sign Out</span>
                 </div>
             </div>
 

08cce3e3122285fe734f625a17cab05ca57f1b22
NTiger07 <falaleru@gmail.com>
2023-10-30 14:33:03 +0100

    Auth WIP-1

--- src/components/Auth/LoginContainer.tsx
+++ src/components/Auth/LoginContainer.tsx
@@ -1,7 +1,7 @@
 const LoginContainer = () => {
 
     const handleAuth = () => {
-        const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID; 
+        const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
         const redirectUri = 'http://localhost:3000/auth/google/callback';
         const scope = 'email profile';
         const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
@@ -13,7 +13,8 @@ const LoginContainer = () => {
         <div>
             <button type="button" className="login-with-google-btn" onClick={handleAuth}>
                 Sign in with Google
-            </button></div>
+            </button>
+        </div>
     )
 }
 
--- src/components/HeadBoard.tsx
+++ src/components/HeadBoard.tsx
@@ -1,6 +1,12 @@
 const HeadBoard = () => {
+  const logout = () => {
+    window.location.href = "http://localhost:3000/auth/logout";
+  }
   return (
-    <div className="h-[100%]">HeadBoard</div>
+    <div className="h-[100%]">
+      HeadBoard
+      <button onClick={logout}>LOGOUT</button>
+    </div>
   )
 }
 

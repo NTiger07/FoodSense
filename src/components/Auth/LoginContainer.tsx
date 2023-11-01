f15ece03216905ca7fc274a435cfa4312cf471ac
NTiger07 <falaleru@gmail.com>
2023-11-01 17:01:36 +0100

    Login Logic bare bone DONE

--- src/components/Auth/LoginContainer.tsx
+++ src/components/Auth/LoginContainer.tsx
@@ -1,4 +1,35 @@
+import axios from "axios"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
+
 const LoginContainer = () => {
+
+    const navigate = useNavigate()
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const [visible, setVisible] = useState(false)
+
+    const userCredentials = {
+        username, password
+    }
+
+    const authenticateUser = () => {
+        axios
+            .post("http://localhost:3000/auth/login", userCredentials)
+            .then((res) => {
+                console.log(res)
+                navigate("/dashboard")
+            })
+            .catch((err) => {
+                console.error(err)
+                toast.error("Error Logging in")
+            })
+    }
+
+
     return (
         <div className="flex justify-center items-center min-h-screen overflow-y-hidden">
             <div className="bg-light-black rounded-lg p-8 shadow w-[30%] h-[60vh] overflow-y-hidden">
@@ -7,17 +38,20 @@ const LoginContainer = () => {
                     <input
                         type="text"
                         placeholder="Email"
-                        className="w-full h-12 border-2 border-white border-opacity-20 rounded-full px-4 text-black"
+                        className="w-full h-12 outline-none border-none border-white border-opacity-20 rounded-full px-4 text-black"
+                        onChange={(e) => setUsername(e.target.value)}
                         required
                     />
                 </div>
-                <div className="mb-6">
+                <div className="mb-6 flex justify-between flex-row w-full h-12 px-[1rem] rounded-full text-black bg-white">
                     <input
-                        type="password"
+                        type={visible ? "text" : "password"}
                         placeholder="Password"
-                        className="w-full h-12 border-2 border-white border-opacity-20 rounded-full px-4 text-black"
+                        className="outline-none border-none px-4 w-[80%] bg-white"
+                        onChange={(e) => setPassword(e.target.value)}
                         required
                     />
+                    <img src={`/assets/eye${visible ? "":"-off"}-line.svg`} className="w-[7%] cursor-pointer" alt="" onClick={() => setVisible(!visible)} />
                 </div>
                 <div className="flex justify-between text-sm mb-4">
                     <label className="flex items-center text-white">
@@ -27,7 +61,7 @@ const LoginContainer = () => {
                     <a href="#" className="text-white hover:underline">Forgot Password</a>
                 </div>
                 <button
-                    type="submit"
+                    onClick={authenticateUser}
                     className="w-full h-12 bg-white text-black rounded-full font-semibold hover:bg-opacity-90 transition duration-300"
                 >
                     Login

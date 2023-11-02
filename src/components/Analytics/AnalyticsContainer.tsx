0802125093c05136542d4e251e23d8f414f8a2d5
NTiger07 <falaleru@gmail.com>
2023-11-02 19:23:42 +0100

    Waste Trends Graph WIP-0

--- src/components/Analytics/AnalyticsContainer.tsx
+++ src/components/Analytics/AnalyticsContainer.tsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useState } from "react";
+import { ResponsiveLine } from "@nivo/line";
+import  mockData  from "./mockData"
 
 const AnalyticsContainer = (props: any) => {
   const { userData } = props
 
   const [inventoryList, setInventoryList] = useState([])
-  
+
   const storedTrashListJSON = localStorage.getItem("trashList");
   const storedTrashList = JSON.parse(storedTrashListJSON) || []
 
@@ -20,6 +22,47 @@ const AnalyticsContainer = (props: any) => {
         console.error(err)
       })
   }
+
+  const MyResponsiveLine = ({ data }) => (
+    <ResponsiveLine
+      data={data}
+      margin={{ top: 7, right: 30, bottom: 70, left: 60 }}
+      xScale={{ type: 'point' }}
+      yScale={{
+        type: 'linear',
+        min: 'auto',
+        max: 'auto',
+        stacked: true,
+        reverse: false
+      }}
+      yFormat=" >-.2f"
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'Date',
+        legendOffset: 36,
+        legendPosition: 'middle'
+      }}
+      axisLeft={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'Units',
+        legendOffset: -40,
+        legendPosition: 'middle'
+      }}
+      enablePoints={false}
+      pointSize={10}
+      pointColor={{ theme: 'background' }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: 'serieColor' }}
+      pointLabelYOffset={-12}
+      useMesh={true}
+    />
+  )
 
   console.log(storedTrashList)
 
@@ -36,7 +79,9 @@ const AnalyticsContainer = (props: any) => {
 
 
         <div className="MAINGRAPHS flex flex-row items-cente h-[60%] gap-4">
-          <div className="TRENDS w-[75%] h-[100%] bg-[#37474F] rounded-lg shadow-lg">Waste Trends</div>
+          <div className="TRENDS w-[75%] h-[100%] bg-[#fff] rounded-lg shadow-lg">Waste Trends
+            <MyResponsiveLine data={mockData} />
+          </div>
           <div className="TYPES_PIE w-[30%] h-[100%] bg-[#37474F] rounded-lg shadow-lg">Waste Type Pie</div>
         </div>
 
---
+++ src/components/Analytics/mockData.ts
@@ -0,0 +1,59 @@
+const mockData = [
+  {
+    id: "japan",
+    color: "hsl(1, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 199,
+      },
+      {
+        x: "helicopter",
+        y: 194,
+      },
+      {
+        x: "boat",
+        y: 282,
+      },
+      {
+        x: "train",
+        y: 219,
+      },
+      {
+        x: "subway",
+        y: 229,
+      },
+      {
+        x: "bus",
+        y: 15,
+      },
+      {
+        x: "car",
+        y: 111,
+      },
+      {
+        x: "moto",
+        y: 182,
+      },
+      {
+        x: "bicycle",
+        y: 82,
+      },
+      {
+        x: "horse",
+        y: 244,
+      },
+      {
+        x: "skateboard",
+        y: 154,
+      },
+      {
+        x: "others",
+        y: 153,
+      },
+    ],
+  },
+];
+
+
+export default mockData

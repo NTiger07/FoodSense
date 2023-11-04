d12b498aae03e6f311ac95b206bdc8d89cdc4f4f
NTiger07 <falaleru@gmail.com>
2023-11-04 16:28:23 +0100

    Analytics Charts Data

--- src/components/Analytics/AnalyticsContainer.tsx
+++ src/components/Analytics/AnalyticsContainer.tsx
@@ -24,21 +24,21 @@ const AnalyticsContainer = (props: any) => {
 
 
   return (
-    <div className="mx-[2%] mt-[2%] flex flex-col gap-2 overflow-scroll">
+    <div className="mx-[2%] mt-[2%] flex flex-col gap-2 overflow-hidden">
       <div className="SEARCH h-[10vh]">
         Header and account
       </div>
 
       <div className="MAINSECTION h-content flex flex-col gap-7">
         <div className="MAINGRAPHS flex flex-row items-cente h-[50vh] gap-4">
-          <WasteTrends data={mockData2} />
+          <WasteTrends data={trashList} />
           {/* <div className="TYPES_PIE w-[30%] h-[100%] bg-[#37474F] rounded-lg shadow-lg">Waste Type Pie</div> */}
         </div>
 
 
         <div className="h-[40vh] gap-4 flex items-center mb-[2rem]">
-          <Progress data={mockData2} />
-          <Impact data={mockData2}/>
+          <Progress data={trashList} />
+          <Impact data={trashList} />
           {/* <div className="MONTHLY SUMMARY w-[33%] rounded-lg shadow-lg bg-[#37474F] h-[100%]"></div> */}
         </div>
 
--- src/components/Analytics/Impact.tsx
+++ src/components/Analytics/Impact.tsx
@@ -1,8 +1,15 @@
+import axios from "axios"
+
 const Impact = (props: any) => {
   const { data } = props
+  const postKini = () => {
+    for (let i = 0; i < 51; i++){
 
+      axios.post(`http://localhost:3000/items/trash/${data[i]._id}`)
+    }
+  }
   return (
-    <div className="IMPACT w-[40%] rounded-lg shadow-lg bg-[#37474F] h-[100%]"></div>
+    <div className="IMPACT w-[40%] rounded-lg shadow-lg bg-[#37474F] h-[100%]" onClick={postKini}></div>
   )
 }
 

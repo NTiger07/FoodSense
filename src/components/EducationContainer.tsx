70f9e74919647644ad13ed1134da2f848a1dea31
NTiger07 <falaleru@gmail.com>
2023-11-09 16:43:56 +0100

    Fixed coming soon lag

--- src/components/EducationContainer.tsx
+++ src/components/EducationContainer.tsx
@@ -1,7 +1,7 @@
 const EducationContainer = () => {
   return (
     <div className="flex items-center justify-center h-[100%] flex-col">
-      <span className="bg-[url('/images/soon.jpg')] bg-clip-text bg-cover text-transparent font-extrabold text-[7rem]">COMING SOON</span>
+      <span className="bg-[url('/images/soon.jpg')] bg-clip-text bg-contain text-transparent font-extrabold text-[7rem]">COMING SOON</span>
       <p className="w-[60%]">
         At FoodSense, we believe that knowledge is the first step toward making meaningful changes.
         That's why we're thrilled to introduce the Education Section –

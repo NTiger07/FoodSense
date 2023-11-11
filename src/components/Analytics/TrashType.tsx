acb0a6f4145c53593319aba67bf8fe15c6874164
NTiger07 <falaleru@gmail.com>
2023-11-11 19:48:31 +0100

    TrashType PieChart DONE

--- src/components/Analytics/TrashType.tsx
+++ src/components/Analytics/TrashType.tsx
@@ -1,10 +1,14 @@
 import axios from "axios"
-import { useState } from "react"
-import { ResponsiveContainer, PieChart } from "recharts"
+import { useEffect, useState } from "react"
+import { ResponsiveContainer, PieChart, Pie, Legend, Cell, Tooltip } from "recharts"
 
 const TrashType = (props: any) => {
   const { userData } = props
-  const [response, setResponse] = useState({})
+  const [response, setResponse] = useState([])
+
+  useEffect(() => {
+    getType()
+  }, [])
 
   const getType = () => {
     axios.get(`${import.meta.env.VITE_LOCAL_URL}trash/type/${userData.id}`)
@@ -16,11 +20,21 @@ const TrashType = (props: any) => {
       })
   }
 
+  const COLORS = ['#d12b2f', '#4A6CF7']
+
   return (
-    <div className="IMPACT w-[30%] rounded-lg shadow-lg bg-[#37474F] h-[100%]" onClick={getType}>
-      {/* <ResponsiveContainer width="100%" height="100%">
-       <PieChart data={response} /> 
-      </ResponsiveContainer> */}
+    <div className="TRASHTYPE w-[30%] rounded-lg shadow-lg bg-[#37474F] h-[100%]">
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart margin={{ top: 15, right: 5, bottom: 5, left: 5 }}>
+          <Pie data={response} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={70} fill="#82ca9d">
+            {
+              response.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]} />)
+            }
+          </Pie>
+          <Legend />
+          <Tooltip formatter={(str) => str + '%'} />
+        </PieChart>
+      </ResponsiveContainer>
     </div>
   )
 }

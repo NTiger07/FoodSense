9e0e8a4e080ffa1e930dbd3909e8c6e3515dea5e
NTiger07 <falaleru@gmail.com>
2024-01-02 23:04:40 +0100

    Search Logic WIP

--- src/components/Inventory/InventoryContainer.tsx
+++ src/components/Inventory/InventoryContainer.tsx
@@ -77,7 +77,90 @@ const InventoryContainer = (props: any) => {
 
           <div className="ITEMS flex flex-col h-fit w-[65%] bg-[#00B074]">
 
+<div className="SEARCH">
+              <div className="flex h-[1rem] flex-row items-center justify-between my-[5%]">
+                <div className="flex h-[100%] flex-row items-center">
+                  <div className="h-[2.5rem] cursor-pointer rounded-6xs mr-[5%] px-[5%] bg-whites-plain box-border overflow-hidden flex flex-row items-center justify-center border border-greys-etherium">
+                    <img className="" alt="" src="/icons/arrows-exchange-alt-v.svg" />
+                  </div>
 
+                  <div className="w-[100%] h-[2.5rem] py-[5px] md:py-[13px] px-[15px] items-center justify-start flex flex-row bg-whites-plain rounded-6xs shadow-[0px_1px_2px_rgba(28,_39,_49,_0.08)] box-border border-[1px] border-solid border-greys-etherium">
+                    <img src="/icons/search-icon.svg" alt="" />
+                    <input
+                      className="font-work-sans pl-[20px] w-[100%] text-[1.2rem] outline-none appearance-none"
+                      type="search"
+                      placeholder="Search"
+                    // value={searchValueProp}
+                    // onChange={(e) => setSearchValueProp(e.target.value)}
+                    // onKeyUp={(e) => {
+                    //     if (e.key === "Enter") {
+                    //         apiFunction();
+                    //     }
+                    //     if (searchValueProp == ""){
+                    //         apiFunction();
+                    //     }
+                    // }}
+                    />
+                  </div>
+                  {/* <div className="w-[100%] h-[2.5rem]">
+
+                    <TextField id="outlined-basic" label="Search" variant="outlined" />
+                </div> */}
+                </div>
+
+                <div className="flex text-base h-[100%] w-[40%] justify-end flex-row items-center">
+                  <div className="rounded-3xs ml-[5%] px-[3%] w-[35%] h-[2.5rem] bg-whites-plain box-border flex flex-row items-center justify-between border border-greys-etherium">
+                    <div className="relative tracking-[0.02rem] leading-[1.2rem] w-max">
+                      Show All
+                    </div>
+                    <div className="flex flex-row items-center justify-center">
+                      <img className="relative" alt="" src="/icons/two-arrows.svg" />
+                    </div>
+                  </div>
+                  <div className="relative rounded-3xs ml-[5%] px-[3%] h-[2.5rem] bg-whites-plain box-border flex flex-row  items-center justify-center border border-greys-etherium">
+                    <div className="flex items-center cursor-pointer">
+                      <div className="overflow-hidden flex flex-row p-[3px] items-center justify-center gap-[8px]">
+                        <img
+                          className="relative w-[18.01px] h-[18.01px]"
+                          alt=""
+                          src="/icons/calendar.svg"
+                        />
+                        <div className="relative box-border w-px h-[13px] border-r-[1px] border-text-black mr-[5px]" />
+                      </div>
+                      <div className="relative tracking-[0.02em] leading-[120%] inline-block w-[89px] shrink-0">
+                        Date
+                      </div>
+                      <div className="overflow-hidden flex flex-col py-[5.5px] px-0.5 items-center justify-center">
+                        <img
+                          className="relative w-2.5 h-[6.5px]"
+                          alt=""
+                          src="/icons/one-arrow-down.svg"
+                        />
+                      </div>
+                    </div>
+
+                    {/* <div className={`absolute top-[120%] right-[-50%] flex justify-between gap-3 items-center w-max ${isDateRangeOpen ? " ": "hidden"}`}>
+                          
+
+                        <DatePicker
+                            label="Start Date"
+                            value={startTimeProp}
+                            onChange={handleStartTimeChange}
+                            format="DD-MM-YYYY"
+                        />
+                        <DatePicker
+                            label="End Date"
+                            value={endTimeProp}
+                            onChange={handleEndTimeChange}
+                            format="DD-MM-YYYY"
+                        />
+                        </div> */}
+                  </div>
+
+
+                </div>
+              </div>
+</div>
             <div className="HEADER flex items-center py-[2%] w-full text-light-black font-semibold">
 
               <div className="NAMEID flex w-[30%]">
